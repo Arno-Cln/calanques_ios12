06bcb032ea064c8ac8baf1a3a19f499f9504e8e2
Arno Cln <acaillon@icloud.com>
2019-02-14 08:29:04 +0100

    Initial Commit

---
+++ Les Calanques/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Les Calanques
+//
+//  Created by Arnaud Caillon on 14/02/2019.
+//  Copyright © 2019 Arnaud Caillon. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
