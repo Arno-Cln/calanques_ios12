af54880141bfd3b10a53accd48b5144b99058386
Arno Cln <acaillon@icloud.com>
2019-02-15 10:04:53 +0100

    Custom TableViewCell

--- Les Calanques/Controller/TableViewIntegreeController.swift
+++ Les Calanques/Controller/TableViewIntegreeController.swift
@@ -11,11 +11,17 @@ import UIKit
 class TableViewIntegreeController: UITableViewController {
     
     var calanques: [Calanque] = []
+    var cellID = "CalanqueCell"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         calanques = CalanqueCollection().all()
+        tableView.backgroundColor = UIColor.clear
+        let bg = UIImageView(frame: view.bounds)
+        bg.image = calanques[0].image
+        bg.contentMode = .scaleAspectFill
+        tableView.backgroundView = bg
         
 
         
@@ -37,14 +43,18 @@ class TableViewIntegreeController: UITableViewController {
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        if let cell = tableView.dequeueReusableCell(withIdentifier: cellID) as? CalanqueCell {
+            cell.setupCell(calanques[indexPath.row])
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+            
+            let calanque = calanques[indexPath.row] // Configuration cellules.
+            cell.textLabel?.text = calanque.nom
+            cell.imageView?.image = calanque.image
+            return cell
+        }
         
-        let calanque = calanques[indexPath.row] // Configuration cellules.
-        cell.textLabel?.text = calanque.nom
-        cell.imageView?.image = calanque.image
-
-
-        return cell
     }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
